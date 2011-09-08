6998e7e21602f77099d017e44d1b294ecfc2d33c
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-09-08 18:04:20 -0300

    small refactoring on tests

--- src/test/java/br/maplink/webservices/places/client/wrapper/HexHmacSha1GeneratorImplTest.java
+++ src/test/java/br/maplink/webservices/places/client/wrapper/HexHmacSha1GeneratorImplTest.java
@@ -1,25 +1,17 @@
 package br.maplink.webservices.places.client.wrapper;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
-
 public class HexHmacSha1GeneratorImplTest {
-
-	private HexHmacSha1GeneratorImpl generator;
-	
-	@Before
-	public void setUp() {
-		generator = new HexHmacSha1GeneratorImpl();
-	}
 	
 	@Test
 	public void shouldConvertIntoHexHmacSha1Hash() throws InvalidKeyException, NoSuchAlgorithmException{
-		String hashGenerated = generator.generateFor("my\ncontent", "my-key");
+		String hashGenerated = new HexHmacSha1GeneratorImpl().generateFor("my\ncontent", "my-key");
 		
 		assertThat(hashGenerated).isEqualTo("34cbdcfcaf9495e1c29a5602aa4ab187370f1799");
 	}
