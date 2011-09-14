b3e6b8f1adb3fb25abcba7a888c1c171e98b38cf
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-09-14 14:24:46 -0300

    implemented places resource

--- src/main/java/br/com/maplink/webservices/places/client/wrapper/XmlSerializerWrapperImpl.java
+++ src/main/java/br/com/maplink/webservices/places/client/wrapper/XmlSerializerWrapperImpl.java
@@ -1,21 +1,26 @@
 package br.com.maplink.webservices.places.client.wrapper;
 
+import br.com.maplink.webservices.places.client.resource.AtomLink;
 import br.com.maplink.webservices.places.client.resource.Categories;
 import br.com.maplink.webservices.places.client.resource.Category;
 import br.com.maplink.webservices.places.client.resource.Place;
+import br.com.maplink.webservices.places.client.resource.Places;
 
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.io.xml.QNameMap;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 public class XmlSerializerWrapperImpl implements XmlSerializerWrapper {
 
 	private XStream xstream;
 
 	public XmlSerializerWrapperImpl() {
-		xstream = new XStream(new DomDriver("UTF-8"));
+		xstream = new XStream(new StaxDriver(new QNameMap()));
 		xstream.processAnnotations(Category.class);
 		xstream.processAnnotations(Categories.class);
 		xstream.processAnnotations(Place.class);
+		xstream.processAnnotations(Places.class);
+		xstream.processAnnotations(AtomLink.class);
 	}
 	
 	@Override
