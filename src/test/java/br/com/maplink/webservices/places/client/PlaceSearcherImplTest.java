91a0fe14410d6fb90b7e0cf9cfe2fb04ae18b206
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2011-09-14 16:31:23 -0300

    organized imports

--- src/test/java/br/com/maplink/webservices/places/client/PlaceSearcherImplTest.java
+++ src/test/java/br/com/maplink/webservices/places/client/PlaceSearcherImplTest.java
@@ -1,15 +1,13 @@
 package br.com.maplink.webservices.places.client;
 
 import static org.fest.assertions.Assertions.assertThat;
-
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.anyDouble;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
